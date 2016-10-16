c224ef94da71b908975dd1d6010a5f9e3fcd4f08
TT <official.intsp@gmail.com>
2016-10-16 14:37:29 +0300

    New features
    Print array, save array

--- VSLstAL.py
+++ VSLstAL.py
@@ -5,7 +5,8 @@
 # Twitter: @IntSPstudio
 #|==============================================================|#
 #SETTINGS
-fileName ="sample"
+fileNameIn ="sample"
+fileNameOut ="result"
 fileSeparator =";"
 #READ FILE
 def readFile(name):
@@ -14,7 +15,27 @@ def readFile(name):
 	fileContent = file.read().splitlines()
 	file.close()
 	return fileContent
-#ARRAY HEIGHT
+#WRITE FILE
+def writeFile(array):
+	arrayMaxWidth =0
+	arrayHeight = len(array)
+	arrayLine =""
+	fileName = fileNameOut +".csv"
+	f = open(fileName,"w")
+	for yp in range(0, arrayHeight):
+		arrayWidth = len(array[yp])
+		for xp in range(0, arrayWidth):
+			point = str(array[yp][xp])
+			if arrayLine !="":
+				arrayLine = arrayLine + fileSeparator + point
+			else:
+				arrayLine = point
+		if yp < arrayHeight -1:
+			arrayLine = arrayLine +"\n"
+		f.write(arrayLine)
+		arrayLine =""
+	f.close()
+#ARRAY HEIGHT (CSV STYLE)
 def getArrayWidth(array):
 	arrayMaxWidth =0
 	arrayHeight = len(array)
@@ -24,7 +45,6 @@ def getArrayWidth(array):
 		pointCheck =0
 		for xp in range(0, arrayWidth):
 			point = str(array[yp][xp])
-			
 			if point == fileSeparator:
 				pointCheck =0
 			else:
@@ -35,9 +55,9 @@ def getArrayWidth(array):
 			arrayMaxWidth = pointWidth
 	return arrayMaxWidth
 #MAKE ARRAY
-def makeContentArray():
+def makeContentArray(contentFileName):
 	#CONTENT
-	contentFile = readFile(fileName)
+	contentFile = readFile(contentFileName)
 	contentHeight = len(contentFile)
 	#LIMITS
 	arrayHeight = len(contentFile)
@@ -65,8 +85,29 @@ def makeContentArray():
 				#print(wcl)
 				arrayContent[arrayYP][arrayXP] = wcl
 	return arrayContent
+def printContentArraySimple(array):
+	arrayMaxWidth =0
+	arrayHeight = len(array)
+	arrayPrintMap =""
+	arraySeparatorV ="\n"
+	arraySeparatorH =" "
+	for yp in range(0, arrayHeight):
+		arrayWidth = len(array[yp])
+		for xp in range(0, arrayWidth):
+			point = str(array[yp][xp])
+			if point !="":
+				if xp == 0:
+					arrayPrintMap = arrayPrintMap + point
+				else:
+					arrayPrintMap = arrayPrintMap + arraySeparatorH + point
+		if yp < arrayHeight -1:
+			arrayPrintMap = arrayPrintMap + arraySeparatorV
+	return arrayPrintMap
 #START
 if __name__ == "__main__":
 	#CSV TO 2D ARRAY
-	arrayContent = makeContentArray()
-	print(arrayContent)
+	fileContent = makeContentArray(fileNameIn)
+	fileHeight = len(fileContent)
+	fileWidth = arrayWidth = len(fileContent[0])
+	print(fileHeight, "x", fileWidth)
+	print(printContentArraySimple(fileContent))
