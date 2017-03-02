c036f735b2b4ee6eb22e3b1580edb3fb122c4123
IntSPstudio <official.intsp@gmail.com>
2017-03-03 00:47:37 +0200

    30.9800.217-0303.5.1

---
+++ VSLstAP.py
@@ -0,0 +1,36 @@
+#|==============================================================|#
+# Made by IntSPstudio
+# Project Visual Street
+#
+# Twitter: @IntSPstudio
+#|==============================================================|#
+
+#IMPORT
+import it8c
+import platform
+#MAIN
+if __name__ == "__main__":
+	#Create array
+	platformContent = it8c.dataCreateArray(10,2,"")
+	#Get data
+	platformContent[0][0] ="COMPUTER"
+	platformContent[1][0] ="Name"
+	platformContent[1][1] = platform.node()
+	platformContent[2][0] ="Information"
+	platformContent[2][1] = platform.platform()
+	platformContent[3][0] ="Operating system"
+	platformContent[3][1] = platform.system()
+	platformContent[4][0] ="CPU architecture"
+	platformContent[4][1] = platform.machine()
+	platformContent[5][0] ="CPU information"
+	platformContent[5][1] = platform.processor()
+	platformContent[6][0] ="PYTHON"
+	platformContent[7][0] ="Version"
+	platformContent[7][1] = platform.python_version()
+	platformContent[8][0] ="Build number"
+	platformContent[8][1] = platform.python_build()[0]
+	platformContent[9][0] ="Build date"
+	platformContent[9][1] = platform.python_build()[1]
+	#Print data
+	print(it8c.vslTerminalLine(0,""))
+	print(it8c.dataSmrPrintArray(platformContent," ","",0))
