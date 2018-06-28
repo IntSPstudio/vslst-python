60d961e41c62aadda1e091711e903f37729557c1
IntSPstudio <official.intsp@gmail.com>
2018-06-28 23:39:36 +0300

    81.9800.318-2806.5.1

---
+++ stb/VSLstBF.py
@@ -0,0 +1,72 @@
+#|==============================================================|#
+# Made by IntSPstudio
+# Project Visual Street
+#
+# Twitter: @IntSPstudio
+#|==============================================================|#
+
+#LIBRARIES
+import urllib.request, json 
+#SETTINGS
+findKeyA = "data-original" #KEY
+findKeyB = '"{' #START
+findKeyC = '}"' #STOP
+findKeyD = ":"
+findKeyE = ","
+#LOOP
+if __name__ == "__main__":
+    #GIVE LICENSE
+    ap = "https://www.biltema.fi/auton-varaosahaku/" #URL
+    userLp = input("License plate: ")
+    cp = ""
+    if userLp !="":
+        #URL
+        cp = ap + userLp
+        #GET
+        with urllib.request.urlopen(cp) as url:
+            cacheContent = url.read().decode()
+            cacheContent = cacheContent.replace(" ", "")
+            cacheContent = cacheContent.replace("&quot;","")
+            #CHECK POINTS
+            ap = cacheContent.find(findKeyA)
+            bp = cacheContent.find(findKeyB,ap) +2
+            cp = cacheContent.find(findKeyC,bp)
+            #LOOP
+            if ap > 0:
+                if bp > 0:
+                    if cp > 0:
+                        #TA
+                        arrayContent ={}
+                        mode =1
+                        dp =""
+                        fp =""
+                        gp =""
+                        #TB
+                        for i in range(bp,cp):
+                            ep = cacheContent[i]
+                            #TC
+                            if ep == findKeyD:
+                                fp = dp
+                                dp =""
+                            elif ep == findKeyE:
+                                gp = dp
+                                dp =""
+                                #RULE CHECK
+                                fp = str("").join(i for i in fp if i.isalnum())
+                                gp = str("").join(i for i in gp if i.isalnum())
+                                #OPTIONS
+                                if gp == "true":
+                                    arrayContent[fp] = True
+                                elif gp == "null":
+                                    arrayContent[fp] = None
+                                elif gp == "":
+                                    arrayContent[fp] = None
+                                else:
+                                    arrayContent[fp] = gp
+                            else:
+                                dp = dp + str(ep)
+                        #SAVE RESULTS
+                        aUrl = str("").join(i for i in userLp if i.isalnum()) +"_results.json"
+                        aFile = open(aUrl, "w")
+                        json.dump(arrayContent, aFile)
+                        aFile.close()
