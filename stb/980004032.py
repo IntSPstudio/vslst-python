8283e5cfc482068de2ad28b7c248096aadae342e
IntSPstudio <official.intsp@gmail.com>
2025-12-06 14:36:38 +0200

    1025.010612.9800.4.32

--- stb/980004032.py
+++ stb/980004032.py
@@ -2,72 +2,48 @@
 # Made by IntSPstudio
 # Project Visual Street
 # ID: 980004032
-# Twitter: @IntSPstudio
 #|==============================================================|#
 
 #LIBRARIES
-import urllib.request, json 
+import urllib.request, json, re
 #SETTINGS
-findKeyA = "data-original" #KEY
-findKeyB = '"{' #START
-findKeyC = '}"' #STOP
-findKeyD = ":"
-findKeyE = ","
+url_key_segment = "extraVehicleInformation" #KEY
+url_key_blocka ="["
+url_key_blockb ="]"
+url_start_bracket_point =0
+url_end_bracket_point =1
 #LOOP
 if __name__ == "__main__":
     #GIVE LICENSE
-    ap = "https://www.biltema.fi/auton-varaosahaku/" #URL (Change by country)
-    userLp = input("License plate: ") #ABC-123
-    if userLp !="":
-        cp = ap + userLp #URL
+    url_start = "https://www.biltema.fi/auton-varaosahaku/" #URL (Change by country)
+    user_input = input("License plate: ") #ABC-123
+    if user_input !="":
+        url_get = url_start + user_input #URL
+        result =""
         #GET
-        with urllib.request.urlopen(cp) as url:
-            cacheContent = url.read().decode()
-            cacheContent = cacheContent.replace(" ", "")
-            cacheContent = cacheContent.replace("&quot;","")
+        with urllib.request.urlopen(url_get) as url:
+            user_content = url.read().decode()
             #CHECK POINTS
-            ap = cacheContent.find(findKeyA)
-            bp = cacheContent.find(findKeyB,ap) +2
-            cp = cacheContent.find(findKeyC,bp)
-            #LOOP
-            if ap > 0:
-                if bp > 0:
-                    if cp > 0:
-                        #TA
-                        arrayContent ={}
-                        mode =1
-                        dp =""
-                        fp =""
-                        gp =""
-                        #TB
-                        for i in range(bp,cp):
-                            ep = cacheContent[i]
-                            #TC
-                            if ep == findKeyD:
-                                fp = dp
-                                dp =""
-                            elif ep == findKeyE:
-                                gp = dp
-                                dp =""
-                                #RULE CHECK
-                                fp = str("").join(i for i in fp if i.isalnum())
-                                gp = str("").join(i for i in gp if i.isalnum())
-                                #OPTIONS
-                                if gp == "true":
-                                    arrayContent[fp] = True
-                                elif gp == "null":
-                                    arrayContent[fp] = None
-                                elif gp == "":
-                                    arrayContent[fp] = None
-                                else:
-                                    arrayContent[fp] = gp
-                            else:
-                                dp = dp + str(ep)
-                        #SAVE RESULTS (PRINT OR SAVE)
-                        print(json.dumps(arrayContent, indent=2, sort_keys=True))
-                        """
-                        aUrl = str("").join(i for i in userLp if i.isalnum()) +"_results.json"
-                        aFile = open(aUrl, "w")
-                        json.dump(arrayContent, aFile)
-                        aFile.close()
-                        """
+            url_start_point = user_content.find(url_key_segment)
+            #GET
+            print(url_start_point)
+            for i in range(url_start_point,len(user_content)):
+                url_test_point = user_content[i]
+                if url_end_bracket_point == 1:
+                    if url_test_point == url_key_blocka:
+                         url_start_bracket_point =1
+                    if url_test_point == url_key_blockb:   
+                        url_end_bracket_point =0
+                    if url_start_bracket_point == 1:
+                        result = result + user_content[i]
+            #PARSING
+            pairs = re.findall(r'"title":"(.*?)","value":"(.*?)"', result)
+            result_dict = {title: value for title, value in pairs}
+            result_title = user_input
+            #PRINT
+            for i in result_dict:
+                print (i, "=", result_dict[i])
+            #SAVE
+            file_name = result_title +".json"
+            with open(file_name, "w", encoding="utf-8") as f:
+                f.write(json.dumps(result_dict, ensure_ascii=False, indent=2))
